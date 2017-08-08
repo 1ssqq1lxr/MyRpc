3854ba9f2fa9941d819560490ffb5d56c17c9ea3
17070680 <17070680@CNHQ-17070680T.sn.suning.ad>
2017-08-08 18:58:20 +0800

    1

--- src/main/java/com/it/netty/rpc/zookeeper/TestMain.java
+++ src/main/java/com/it/netty/rpc/zookeeper/TestMain.java
@@ -3,7 +3,7 @@ package com.it.netty.rpc.zookeeper;
 public class TestMain {
 		public static void main(String[] args) {
 			
-			ServiceRegist serviceRegist = new ServiceRegist("127.0.0.1:2181");
+			ServiceRegist serviceRegist = new ServiceRegist("localhost:12181");
 			serviceRegist.register("127.0.0.1:8199");
 		}
 }
