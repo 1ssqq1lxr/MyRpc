747f047290ae79de1a944529089768ff864d8442
17070680 <17070680@CNHQ-17070680T.sn.suning.ad>
2017-08-09 10:27:10 +0800

    1

---
+++ src/main/java/com/it/netty/rpc/serialize/SerializeEnum.java
@@ -0,0 +1,22 @@
+package com.it.netty.rpc.serialize;
+
+public enum SerializeEnum {
+		JDKSERIALIZE(1),
+		PROTOBUFSERIALIZE(2),
+		MARSHELLINGSERIALIZE(3);
+		private int value;
+		SerializeEnum(int value){
+				this.value=value;
+		}
+		
+		public BaseRpcSerialize getSerialBean(){
+			switch (value) {
+			case 1:
+				return  new ByteObjConverter();
+
+			default:
+				break;
+			}
+			return null;
+		}
+}
