4a957f30f0284938f76265e8976d17d8411bf9b4
17070680 <17070680@CNHQ-17070680T.sn.suning.ad>
2017-08-14 20:42:03 +0800

    1

---
+++ src/main/java/com/it/netty/rpc/framework/ZkBeanServiceConsume.java
@@ -0,0 +1,28 @@
+package com.it.netty.rpc.framework;
+
+import java.util.Set;
+
+public class ZkBeanServiceConsume {
+	private String address;
+	private String protocol;
+	private Set<String>  classes;
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getProtocol() {
+		return protocol;
+	}
+	public void setProtocol(String protocol) {
+		this.protocol = protocol;
+	}
+	public Set<String> getClasses() {
+		return classes;
+	}
+	public void setClasses(Set<String> classes) {
+		this.classes = classes;
+	}
+	
+}
