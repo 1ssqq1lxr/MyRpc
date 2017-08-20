2059dab8960f7ec5b92f629317f9636f60e3fb12
lxr <1074455781@qq.com>
2017-08-20 22:48:58 +0800

    2

---
+++ src/main/java/com/it/netty/rpc/NoFindClassException/NoFindClassException.java
@@ -0,0 +1,10 @@
+package com.it.netty.rpc.NoFindClassException;
+
+public class NoFindClassException extends Exception{
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return super.getMessage();
+	}
+	
+}
