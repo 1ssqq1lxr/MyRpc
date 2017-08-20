473bee568ef9e7f56f5ab7e0b8cc413329f3b9df
17070680 <17070680@CNHQ-17070680T.sn.suning.ad>
2017-08-20 10:50:54 +0800

    1

---
+++ src/main/java/com/it/netty/rpc/romote/DeafultNettyClientRemoteConnection.java
@@ -0,0 +1,10 @@
+package com.it.netty.rpc.romote;
+
+public class DeafultNettyClientRemoteConnection extends AbstractNettyRemoteConnetion {
+
+	public DeafultNettyClientRemoteConnection(int port) {
+		super(port);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/it/netty/rpc/romote/DeafultNettyServerRemoteConnection.java
@@ -0,0 +1,10 @@
+package com.it.netty.rpc.romote;
+
+public class DeafultNettyServerRemoteConnection extends AbstractNettyRemoteConnetion {
+
+	public DeafultNettyServerRemoteConnection(int port) {
+		super(port);
+		// TODO Auto-generated constructor stub
+	}
+
+}
