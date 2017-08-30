15701adeccbf8389def5c7d62fff843af19d458d
17070680 <17070680@CNHQ-17070680T.sn.suning.ad>
2017-08-30 10:25:08 +0800

    修改

--- src/main/java/com/it/netty/rpc/annocation/RpcService.java
+++ src/main/java/com/it/netty/rpc/annocation/RpcService.java
@@ -4,11 +4,8 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+@Retention(RetentionPolicy.CLASS)
+@Target({ ElementType.TYPE })
 public @interface RpcService {
-	@Retention(RetentionPolicy.CLASS)
-	@Target({ ElementType.TYPE })
-	public @interface LnkVersion {
-	  
-	}
+
 }
