6a002fe40e94aa4c594116e6859b641cffe5a1d8
17070680 <17070680@CNHQ-17070680T.sn.suning.ad>
2017-09-02 21:08:59 +0800

    v1.0

--- src/test/java/springClient.java
+++ src/test/java/springClient.java
@@ -9,12 +9,29 @@ public class springClient {
 	public static void main(String[] args) throws InterruptedException {
 		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("consume.xml");
 		final PersonService bean = (PersonService) applicationContext.getBean(PersonService.class);
-//		System.out.println(bean.setName("ohyeaj"));;
+//		System.out.println();;
 //		System.out.println(bean.getName().getName());;
 //		long currentTimeMillis = System.currentTimeMillis();
-		for(int i=0;i<300;i++){
-			Person name = bean.getName();
-			System.out.println(name.getName());
+		for(int i=0;i<100;i++){
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					Person name = bean.getName();
+					System.out.println(name.getName());
+				}
+			}).start();
+
+		}
+		for(int i=0;i<100;i++){
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					System.out.println(bean.setName("ohyeaj"));
+				}
+			}).start();
+
 		}
 //		long currentTimeMillis1 = System.currentTimeMillis();
 //		System.out.println("共花费========================="+(currentTimeMillis1-currentTimeMillis));
